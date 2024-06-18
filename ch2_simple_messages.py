dec12867728ed36769da33e24ebc5c1c725abb50
Patrick Olvera <patrickolvera@protonmail.com>
2024-06-18 17:27:45 -0500

    chapter 2 messages!

---
+++ ch2_simple_messages.py
@@ -0,0 +1,8 @@
+message = "Hello Python Crash Course!"
+print(message)
+
+message += " Happy to start learning!"
+print(message)
+
+message = "The language 'Python' is named after Monty Python, not the snake."
+print(message)
