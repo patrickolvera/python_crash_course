1fd2c8b5a488dff44e9cb85f3e4d36e7af05035a
Patrick Olvera <patrickolvera@protonmail.com>
2024-06-26 21:35:31 -0500

    ch.2 finished!

---
+++ ch_2/2-10_favorite_number.py
@@ -0,0 +1,3 @@
+fav_number = 13
+message = f"My favorite number is {fav_number + 1}"
+print(message)
---
+++ ch_2/2-11_adding_comments.py
@@ -0,0 +1,2 @@
+# This is a comment
+###!> Its best practice to describe how my code works lol <!###
